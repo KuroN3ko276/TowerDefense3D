d6734c2479f9c876a32f40a8f65a769b14d02881
KuroN3ko276 <nghaidang276@gmail.com>
2024-11-16 18:31:16 +0700

    Camera and Shop System

--- Tower_Defense3D/Assets/Scripts/Node.cs
+++ Tower_Defense3D/Assets/Scripts/Node.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Node : MonoBehaviour
 {
@@ -7,13 +8,23 @@ public class Node : MonoBehaviour
     private GameObject turret;
     private Renderer rend;
     private Color startColor;
+    BuildManager buildManager;
     private void Start()
     {
         rend = GetComponent<Renderer>();
         startColor = rend.material.color;
+        buildManager = BuildManager.instance;
     }
     private void OnMouseDown() 
     {
+        if(EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+        if(buildManager.GetTurretToBuild() == null)
+        {
+            return;
+        }
         if(turret != null)
         {
             Debug.Log("Can't build there !!! - TODO: Display on screen");
@@ -25,6 +36,14 @@ public class Node : MonoBehaviour
     }
     private void OnMouseEnter() 
     {
+        if(EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+        if(buildManager.GetTurretToBuild() == null)
+        {
+            return;
+        }
         GetComponent<Renderer>().material.color = hoverColor;  
     }
 

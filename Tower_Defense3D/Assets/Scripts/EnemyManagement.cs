76121c7188c0df1f82ab7d351c4202b3c6df8a24
KuroN3ko276 <nghaidang276@gmail.com>
2024-11-20 19:20:21 +0700

    Adding healthbar

--- Tower_Defense3D/Assets/Scripts/EnemyManagement.cs
+++ Tower_Defense3D/Assets/Scripts/EnemyManagement.cs
@@ -1,40 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyManagement : MonoBehaviour
 {
     public float startSpeed = 10f;
-    //[HideInInspector]
     public float speed;
-    public float health = 100;
+    public float startHealth = 100;
+    public float health;
     public int worth = 50;
     public GameObject deathEffect;
+
+    [Header("Unity Staff")]
+    public Image healthBar; // Đảm bảo rằng healthBar là một đối tượng Image
+
     private void Start()
     {
         speed = startSpeed;
+        health = startHealth;
     }
+
     public void TakeDamage(float amount)
     {
         health -= amount;
-        if(health <= 0) 
+        healthBar.fillAmount = health / startHealth;
+
+        if (health <= 0) 
         {
             Die();
         }
     }
+
     public void Slow(float pct)
     {
         speed = startSpeed * (1f - pct);
         Debug.Log("Slowing enemy");
-
     }
+
     private void Die()
     {
         PlayerStats.Money += worth;
-        GameObject effect = (GameObject)Instantiate(deathEffect,transform.position,Quaternion.identity);
+        GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(gameObject);
-        Destroy(effect,5f);
+        Destroy(effect, 5f);
     }
-
-    
 }
